a42e27ff5ba43c520dfc8d87d106d968ceed0af8
Nick Zentmayer <nick_zentmayer@outlook.com>
2023-11-15 18:11:09 -0500

    MORE UDPPPP

--- webserver.ino
+++ webserver.ino
@@ -1,169 +1,30 @@
-#include "WiFi.h"
-#include <AsyncTCP.h>
-#include "ESPAsyncWebServer.h"
-#include "SPIFFS.h"
-#include "ESP_Color.h"
+#include "WiFiUDP.h"
 
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
-AsyncWebSocket ws("/ws");
+IPAddress UDP_ADDRESS = IPAddress(192,168,1,163);
+#define UDP_PORT 6969
 
-String lightStatus = "off";
-void toggleOnOff() {
-  if (lightsOn) {
-    prevLedMode = ledMode;
-    ledMode = 2;
-    lightsOn = 0;
-    lightStatus = "off";
-  } else {
-    ledMode = prevLedMode;
-    lightsOn = 1;
-    lightStatus = "on";
-  }
-  ws.textAll(lightStatus);
+WiFiUDP udp;
+
+void initUDP() {
+  Serial.println("Starting UDP Client.");
+  udp.begin(UDP_PORT);
+  ws.textAll("Connected to UDP");
 }
-
-void startSleep() {
-  Serial.println("starting sleep");
-  WiFi.disconnect();
-  server.end();
-  //setCpuFrequencyMhz(80);
-  sleeping = true;
-  Serial.println("sleep started");
+void sendUDP() {
+    udp.beginPacket(UDP_ADDRESS, UDP_PORT);
+    udp.printf("DB_HIT");
+    
+    udp.endPacket();
 }
-
-void endSleep() {
-  Serial.println("ending sleep");
-  setupWiFi();
-  server.begin();
-  //setCpuFrequencyMhz(80);
-  sleeping = false;
-  Serial.println("sleep ended");
+void sendUDP(String str) {
+    udp.beginPacket(UDP_ADDRESS, UDP_PORT);
+    udp.printf(str.c_str());
+    
+    udp.endPacket();
 }
-
-// Replaces placeholder with LED state value
-String processor(const String& var){
-  return "";
-  if (lightsOn) {
-    return "on";
-  } else {
-    return "off";
-  }
-}
-
-String getBatteryVoltage() {
-  int batV = 0;
-  for(int i = 0; i < 16; i++) {
-    batV = batV + analogReadMilliVolts(A3); // ADC with correction   
-  }
-  float batVCalcd = 2.0 * float(batV) / 16.0 / 1000.0;     // attenuation ratio 1/2, mV --> V
-
-  return "V" + String(batVCalcd, 2);
-}
-
-void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
-  AwsFrameInfo *info = (AwsFrameInfo*)arg;
-  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
-    data[len] = 0;
-    String str = (char*)data;
-    if (str == "toggle") {
-      toggleOnOff();
-    } else if (str == "white") {
-      ledMode = 0;
-      lightsOn = 1;
-      ws.textAll("on");
-    } else if (str == "hue") {
-      ledMode = 1;
-      lightsOn = 1;
-      ws.textAll("on");
-    } else if (str == "rainbow") {
-      ledMode = 3;
-      lightsOn = 1;
-      ws.textAll("on");
-    } else if (str == "pulse") {
-      calibrateMpu();
-      ledMode = 4;
-      lightsOn = 1;
-      ws.textAll("on");
-    } else if (str.substring(0, 1) == "#") { // if hue value {
-      c = ESP_Color::Color::FromHex(str);
-    } else { // must be brightness value
-      brightness = str.toInt();
-    }
-  }
-}
-
-void onWSEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
-             void *arg, uint8_t *data, size_t len) {
-  switch (type) {
-    case WS_EVT_CONNECT:
-      lightStatus = (lightsOn) ? "on" : "off";
-      ws.textAll(lightStatus);
-      ws.textAll(String((int)brightness));
-      ws.textAll(getBatteryVoltage());
-      connectedClients = ws.count();
-      break;
-    case WS_EVT_DISCONNECT:
-      Serial.printf("WebSocket client #%u disconnected\n", client->id());
-      connectedClients = ws.count();
-      break;
-    case WS_EVT_DATA:
-      handleWebSocketMessage(arg, data, len);
-      break;
-    case WS_EVT_PONG:
-    case WS_EVT_ERROR:
-      Serial.println("Web Socket Error!");
-      break;
-  }
-}
-
-void initWebSocket() {
-  Serial.println("Initializing WebSocket.");
-  ws.onEvent(onWSEvent);
-  server.addHandler(&ws);
-}
- 
-void startWebServer() {
-  Serial.println("Starting Web Server");
-  // Initialize SPIFFS
-  if(!SPIFFS.begin(true)){
-    return;
-  }
-
-  Serial.println("Starting mDNS.");
-  if(!MDNS.begin("dartboard")) {
-     Serial.println("Error starting mDNS");
-     return;
-  }
-
-  initWebSocket();  
-
-  // Route for index.html
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-  
-  // Route to load style.css file
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/style.css", "text/css");
-  });
-
-  // Route to load script.js file
-  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/script.js", "text/javascript");
-  });
-
-  // Start server
-  server.begin();
-}
-
-void handleWS(void *parameter) {
+void maint(void* pvParameters) {
   for(;;) {
-    ws.cleanupClients();
-    connectedClients = ws.count();
-    if (connectedClients > 0) {
-      //ws.textAll(getBatteryVoltage());
-    }
-    vTaskDelay(1000);
+    sendUDP(" ");
+    delay(10);
   }
 }
